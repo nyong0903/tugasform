4ae39844ba6584864591c482c42d8915ac469759
nyong0903 <nyongbramasta@gmail.com>
2024-11-01 10:03:03 +0700

    Add files via upload

---
+++ form/p_form.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas Form</title>
+
+    <style>
+        .table-active{
+        align-items : center;
+        }
+            
+    </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+<table class="table table-dark table-bordered" >
+
+<tbody>
+  <tr class="table-active">
+  <td style ="text-align : center;">
+        Name
+    </td>
+    
+    <td style ="text-align : center;">
+    My ages
+   </td>
+
+   <td style ="text-align : center;">
+  Gender
+   </td>
+
+   <td style ="text-align : center;">
+    Education
+   </td>
+
+   <td style ="text-align : center;">
+  Hobby
+   </td>
+</tr>
+
+  <tr>
+  <td>
+    Arlan
+  </td>
+   
+  <td style ="text-align : center;">
+    21 tahun
+  </td>
+
+  <td style ="text-align : center;">
+    Laki-laki
+  </td>
+
+  <td style ="text-align : center;">
+    S3
+  </td>
+
+  <td style ="text-align : center;">
+    Olahraga 
+  </td>
+  </tr>
+ 
+  <tr>
+    
+    <td colspan="5" class="table-active"><a href="form.php">
+    <button style = "color : blue;">Kembali</button></a>
+ </td>
+  
+</tbody>
+</table>
+
+</body>
+</html>
